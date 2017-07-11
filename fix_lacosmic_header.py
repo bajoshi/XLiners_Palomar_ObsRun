1f8a29b64e248e5c75bf2e7410544bdb7de816b6
Bhavin Joshi <bajoshi@asu.edu>
2017-07-11 10:17:35 -0700

    added notes on reduction and XLINERS list and other minor updates

--- fix_lacosmic_header.py
+++ fix_lacosmic_header.py
@@ -1,7 +1,11 @@
 from astropy.io import fits
 
-orig_name = '/Volumes/Bhavins_backup/ipac/Palomar_data/2017/20170511/tspec0026.fits'
-clean_name = '/Volumes/Bhavins_backup/ipac/Palomar_data/2017/20170511/tspec0026_cleaned.fits'
+ext_dir = '/Volumes/Bhavins_backup/ipac/Palomar_data/2017/'
+date = '20170509/'
+filename = 'xl53_BA_median'
+
+orig_name = ext_dir + date + filename + '.fits'
+clean_name = ext_dir + date + filename + '_cleaned.fits'
 
 orig = fits.open(orig_name)
 cleaned = fits.open(clean_name)
